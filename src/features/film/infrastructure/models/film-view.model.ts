f542a6151e1da30478bde184acdce60b419c90ef
8blank8 <roc.32@yandex.ru>
2023-12-23 21:27:49 +0700

    add file: film-view.model

---
+++ src/features/film/infrastructure/models/film-view.model.ts
@@ -0,0 +1,12 @@
+
+export class FilmViewModel {
+    title: string;
+    posterUrl: string;
+    rating: string;
+    genre: string[];
+    year: string;
+    description: string;
+    type: string;
+    id: string;
+    kinopoiskId: string;
+}
