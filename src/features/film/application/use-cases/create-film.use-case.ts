2d46db4d6da36d82374964a57a5028bd17f7ea05
8blank8 <roc.32@yandex.ru>
2023-12-23 21:26:26 +0700

    feat: return kinopoiskId

--- src/features/film/application/use-cases/create-film.use-case.ts
+++ src/features/film/application/use-cases/create-film.use-case.ts
@@ -1,16 +1,19 @@
 import { CommandHandler } from "@nestjs/cqrs";
 import { CreateFilmCommand, Film } from "../../domain/film";
 import { FilmRepository } from "../../infrastructure/film.repository";
+import { KinopoiskAdapter } from "../../../../adapter/kinopoisk.adapter";
 
 
 @CommandHandler(CreateFilmCommand)
 export class CreateFilmUseCase {
-    constructor(private filmRepo: FilmRepository) { }
+    constructor(
+        private filmRepo: FilmRepository,
+    ) { }
 
     async execute(dto: CreateFilmCommand): Promise<string> {
         const film = Film.create(dto)
         this.filmRepo.save(film)
 
-        return film.id
+        return film.kinopoinskId
     }
 }
