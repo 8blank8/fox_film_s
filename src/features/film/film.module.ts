cccea363a12d81d815b00db4d1f36ceddd051da1
8blank8 <roc.32@yandex.ru>
2023-12-23 21:25:56 +0700

    feat: add modules

--- src/features/film/film.module.ts
+++ src/features/film/film.module.ts
@@ -5,6 +5,9 @@ import { CreateFilmUseCase } from './application/use-cases/create-film.use-case'
 import { FilmRepository } from './infrastructure/film.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Film } from './domain/film';
+import { FilmQueryRepository } from './infrastructure/film.query.repository';
+import { CqrsModule } from '@nestjs/cqrs';
+import { KinopoiskAdapter } from '../../adapter/kinopoisk.adapter';
 
 const useCases = [
   CreateFilmUseCase
@@ -12,12 +15,15 @@ const useCases = [
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Film])
+    TypeOrmModule.forFeature([Film]),
+    CqrsModule
   ],
   controllers: [FilmController],
   providers: [
     FilmService,
     FilmRepository,
+    FilmQueryRepository,
+    KinopoiskAdapter,
     ...useCases
   ],
 })
