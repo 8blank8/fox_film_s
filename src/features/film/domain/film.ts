21db01ea1e5110745a98e92bab6a432278c862b4
8blank8 <roc.32@yandex.ru>
2023-12-23 21:26:58 +0700

    feat: add column kinopoiskId

--- src/features/film/domain/film.ts
+++ src/features/film/domain/film.ts
@@ -1,19 +1,27 @@
 import { randomUUID } from "crypto"
 import { DefaultEntity } from "../../../core/default.entity"
 import { Column, Entity } from "typeorm"
+import { FilmTypeEnum } from "./enum/film-type.enum"
+import { FilmViewModel } from "../infrastructure/models/film-view.model"
 
 export class CreateFilmCommand {
-    public title: string
-    public posterUrl: string
-    public rating: string
-    public genre: string[]
-    public year: string
-    public decsription: string
-    public type: FilmType
+    constructor(
+        public title: string,
+        public posterUrl: string,
+        public rating: string,
+        public genre: string[],
+        public year: string,
+        public description: string,
+        public type: "FILM" | "TV_SERIES",
+        public kinopoiskId: string
+    ) { }
 }
 
 @Entity()
 export class Film extends DefaultEntity {
+
+    @Column()
+    kinopoinskId: string
 
     @Column()
     title: string
@@ -34,24 +42,23 @@ export class Film extends DefaultEntity {
     description: string
 
     @Column()
-    type: FilmType
+    type: "FILM" | "TV_SERIES"
 
     static create(command: CreateFilmCommand) {
+        // TODO: пофиксить тип, возможно придется передать команду 
+        const { title, description, genre, posterUrl, rating, type, year, kinopoiskId } = command
         const film = new Film()
-        film.title = command.title
-        film.description = command.decsription
-        film.genre = command.genre
+        film.title = title
+        film.description = description
+        film.genre = genre
         film.id = randomUUID()
-        film.posterUrl = command.posterUrl
-        film.rating = command.rating
-        film.type = command.type
-        film.year = command.year
+        film.posterUrl = posterUrl
+        film.rating = rating
+        film.type = type
+        film.year = year
+        film.kinopoinskId = kinopoiskId
 
         return film
     }
 }
 
-enum FilmType {
-    FILM = 0,
-    TV_SERIES = 1
-}
